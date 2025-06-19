1c3c36adececfd4c5b51ad87e692d1174cf1de52
SawyerChristensen <schr5498@gmail.com>
2025-06-19 10:04:11 -0700

    Deleted Storyboards & Hello World

--- DeckedOut MessagesExtension/MessagesViewController.swift
+++ DeckedOut MessagesExtension/MessagesViewController.swift
@@ -12,6 +12,20 @@ class MessagesViewController: MSMessagesAppViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
         // Do any additional setup after loading the view.
     }
     
